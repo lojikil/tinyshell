2e462485f8ae5dfeefa1400e652a715038963416
Rahul Zhade <rzhade3@users.noreply.github.com>
2022-11-08 20:50:22 +0000

    Update to be usable as a library

--- shell.py
+++ shell.py
@@ -1,14 +1,16 @@
 import sys
 import urllib.parse, urllib.request
 
-endpoint = sys.argv[1]
-
-while True:
-    line = input("$ ")
-    line = line.strip()
-    if line == "quit" or line == "exit":
-        sys.exit(0)
-    encline = urllib.parse.quote(line, safe='')
+def run_command(endpoint, command):
+    encline = urllib.parse.quote(command, safe='')
     response = urllib.request.urlopen(endpoint + "?c=" + encline)
-    data = response.read()
-    print(data.decode('ascii'))
+    return response.read().decode('utf-8')
+
+if __name__ == "__main__":
+    endpoint = sys.argv[1]
+    while True:
+        line = input("$ ")
+        line = line.strip()
+        if line == "quit" or line == "exit":
+            sys.exit(0)
+        print(run_command(endpoint, line))
