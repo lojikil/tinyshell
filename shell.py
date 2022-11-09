7bd3e49e5c610b7e66e7e87508d072a5ef52e82a
Stefan Edwards <lojikil@github.com>
2022-11-09 07:22:10 -0500

    added a bunch of functionality, basically allowing the user to change the parameter name, and add extra parameters (such as a `key`)

--- shell.py
+++ shell.py
@@ -1,16 +1,46 @@
 import sys
 import urllib.parse, urllib.request
 
-def run_command(endpoint, command):
+def run_command(endpoint, command, paramname=None, extraparams=None):
     encline = urllib.parse.quote(command, safe='')
-    response = urllib.request.urlopen(endpoint + "?c=" + encline)
+    pname = "c"
+
+    if paramname is not None:
+        pname = paramname
+
+    # smoosh everything together here into a single URL
+    # including any extra parameters the user may have
+    # requested we include
+    query = endpoint + "?{0}={1}".format(pname, encline)
+
+    if extraparams is not None:
+        query = "{0}&{1}".format(query, extraparams)
+
+    response = urllib.request.urlopen(query)
     return response.read().decode('utf-8')
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("usage: shell endpoint [parameter name] [extraparams]")
+        sys.exit(0)
+
     endpoint = sys.argv[1]
+
+    paramname = None
+    extraparams = None
+
+    # intentionally done as two `if` forms
+    # the first pulls out a custom parameter name
+    # the second, any extra data we wish to include
+    if len(sys.argv) > 2:
+        paramname = sys.argv[2]
+
+    if len(sys.argv) > 3:
+        extraparams = sys.argv[3]
+
     while True:
         line = input("$ ")
         line = line.strip()
         if line == "quit" or line == "exit":
             sys.exit(0)
-        print(run_command(endpoint, line))
+        print(run_command(endpoint, line, paramname=paramname, extraparams=extraparams))
