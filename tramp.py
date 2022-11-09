5f5779b82e7099a59a237e4fed2e8d2d84d3daf7
Stefan Edwards <lojikil@github.com>
2022-11-09 07:38:31 -0500

    updated tramp to use the new `run_command` from shell, need to do a bit more work

--- tramp.py
+++ tramp.py
@@ -1,24 +1,23 @@
 import sys
-import urllib
-import urllib2
+import base64
+from .shell import run_command
 
-endpoint = sys.argv[1]
-dest = sys.argv[2]
+def tramp_send(endpoint, dest, paramname, extraparams):
+    with open(dest, 'r') as fh:
+        for line in fh:
+            data = base64.b64encode(line).strip()
+            cmd = "echo '{0}' >> tmp.{1}".format(data, dest)
+            res = run_command(cmd, endpoint, paramname=paramname, extraparams=extraparams)
+            print(res)
 
-with open(sys.argv[2], 'r') as fh:
-    for line in fh:
-        data = line.encode('base64').strip()
-        cmd = "echo '{0}' >> tmp.{1}".format(data, dest)
-        enccmd = urllib.quote(cmd)
-        response = urllib2.urlopen(endpoint + "?c=" + enccmd)
-        data = response.read()
-        print data
+    # easily could be turned into a loop really
+    cmd = "base64 -d tmp.{0} > {0}".format(dest)
+    res = run_command(cmd, endpoint, paramname=paramname, extraparams=extraparams)
+    print(res)
+    cmd = "rm tmp.{0}".format(dest)
+    res = run_command(res, endpoint, paramname=paramname, extraparams=extraparams)
+    print(res)
 
-enccmd = urllib.quote("base64 -d tmp.{0} > {0}".format(dest))
-response = urllib2.urlopen(endpoint + "?c=" + enccmd)
-data = response.read()
-print data
-enccmd = urllib.quote("rm tmp.{0}".format(dest))
-response = urllib2.urlopen(endpoint + "?c=" + enccmd)
-data = response.read()
-print data
+
+if __name__ == "__main__":
+    print("tramp endpoint dest [parameter name] [extra parameters]")
